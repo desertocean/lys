fcd7c3fd2e5f36406c77765f6a93bcfaf61de55a
desertocean <desertwreckage@outlook.com>
2026-01-30 13:21:58 +0800

    update

---
+++ InputDebugger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputDebugger : MonoBehaviour
+{
+    private PlayerInputActions inputActions;
+
+    private void Start()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.Enable();
+
+        inputActions.Movement.Move.performed += ctx =>
+        {
+            var value = ctx.ReadValue<Vector2>();
+            Debug.Log($"移动: {value}");
+        };
+
+        inputActions.Movement.Jump.performed += ctx =>
+        {
+            Debug.Log("跳跃按下");
+        };
+    }
+}
