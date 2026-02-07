bfba6529ae2e8a56e46fc260b2770f515ff735c0
desertocean <desertwreckage@outlook.com>
2026-02-07 16:40:37 +0800

    update

---
+++ JU/EmeraldWaypointObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EmeraldAI
+{
+    public class EmeraldWaypointObject : ScriptableObject
+    {
+	    public List<Vector3> Waypoints = new List<Vector3>();
+	    public List<WaypointExtend> WaypointExtends = new List<WaypointExtend>();
+    }
+}
